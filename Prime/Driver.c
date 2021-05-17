b7607173c88935e226bcbeb9ad7ed5a266c8c950
Sagi Dana <sagidana1@gmail.com>
2021-05-17 10:47:51 +0300

    first

---
+++ Prime/Driver.c
@@ -0,0 +1,56 @@
+#include <ntddk.h>
+#include <wdf.h>
+
+DRIVER_INITIALIZE DriverEntry;
+EVT_WDF_DRIVER_DEVICE_ADD PrimeEvtDeviceAdd;
+
+NTSTATUS
+DriverEntry(
+    _In_ PDRIVER_OBJECT     DriverObject,
+    _In_ PUNICODE_STRING    RegistryPath
+)
+{
+    // NTSTATUS variable to record success or failure
+    NTSTATUS status = STATUS_SUCCESS;
+
+    // allocate the driver configuration object
+    WDF_DRIVER_CONFIG config;
+
+    // Initialize the driver configuration object to register the
+    // entry point for the EvtDeviceAdd callback, PrimeEvtDeviceAdd
+    WDF_DRIVER_CONFIG_INIT(&config,
+        PrimeEvtDeviceAdd
+    );
+
+    // Finally, create the driver object
+    status = WdfDriverCreate(DriverObject,
+        RegistryPath,
+        WDF_NO_OBJECT_ATTRIBUTES,
+        &config,
+        WDF_NO_HANDLE
+    );
+    return status;
+}
+
+NTSTATUS
+PrimeEvtDeviceAdd(
+    _In_    WDFDRIVER       Driver,
+    _Inout_ PWDFDEVICE_INIT DeviceInit
+)
+{
+    // We're not using the driver object,
+    // so we need to mark it as unreferenced
+    UNREFERENCED_PARAMETER(Driver);
+
+    NTSTATUS status;
+
+    // Allocate the device object
+    WDFDEVICE hDevice;
+
+    // Create the device object
+    status = WdfDeviceCreate(&DeviceInit,
+        WDF_NO_OBJECT_ATTRIBUTES,
+        &hDevice
+    );
+    return status;
+}
